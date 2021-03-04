291931e00e428402266964152254ebe77cf9897d
Bill Condo <bill@billcondo.com>
2021-03-04 08:29:36 -0500

    Style CI Config Update (#8)
    * StyleCI Config Update
    
    * Apply fixes from StyleCI
    
    Co-authored-by: Bill Condo <mavrck@users.noreply.github.com>

--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Venue extends Model
